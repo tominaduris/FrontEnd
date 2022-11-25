81e3022ee2c799d96a2b4d56754bd8d0e05b0980
tominaduris <tominaduris@hotmail.com>
2022-11-24 23:33:02 -0300

    Security en el backend

---
+++ naduristomas/BackEnd/src/main/java/com/portfolio/nt/security/Repository/iRolRepository.java
@@ -0,0 +1,12 @@
+package com.portfolio.nt.security.Repository;
+
+import com.portfolio.nt.security.Entity.Rol;
+import com.portfolio.nt.security.Enums.RolNombre;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface iRolRepository extends JpaRepository<Rol, Integer>{
+    Optional<Rol> findByRolNombre(RolNombre rolNombre);
+}
---
+++ naduristomas/BackEnd/src/main/java/com/portfolio/nt/security/Service/UserDetailsImpl.java
@@ -0,0 +1,24 @@
+
+package com.portfolio.nt.security.Service;
+
+import com.portfolio.nt.security.Entity.Usuario;
+import com.portfolio.nt.security.Entity.UsuarioPrincipal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsImpl implements UserDetailsService{
+    @Autowired
+    UsuarioService usuarioService;
+
+    @Override
+    public UserDetails loadUserByUsername(String nombreUsuario) throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.getByNombreUsuario(nombreUsuario).get();
+        return UsuarioPrincipal.build(usuario);
+    }
+    
+    
+}
