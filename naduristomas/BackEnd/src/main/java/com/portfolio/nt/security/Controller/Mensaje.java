bc1e453601f71cdd91b64aa1379b3bfa50d76d9b
tominaduris <tominaduris@hotmail.com>
2022-11-27 13:52:26 -0300

    seguridad en el back end

---
+++ naduristomas/BackEnd/src/main/java/com/portfolio/nt/security/Controller/Mensaje.java
@@ -0,0 +1,26 @@
+
+package com.portfolio.nt.security.Controller;
+
+
+public class Mensaje {
+    private String mensaje;
+    
+    //Constructor
+
+    public Mensaje() {
+    }
+
+    public Mensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+    //Getter and Setter
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+    
+}
--- naduristomas/BackEnd/src/main/java/com/portfolio/nt/security/Service/UsuarioService.java
+++ naduristomas/BackEnd/src/main/java/com/portfolio/nt/security/Service/UsuarioService.java
@@ -19,11 +19,11 @@ public class UsuarioService {
         
     }
     
-    public boolean existByNombreUsuario(String nombreUsuario) {
+    public boolean existsByNombreUsuario(String nombreUsuario) {
         return iusuarioRepository.existsByNombreUsuario(nombreUsuario);
     }
     
-    public boolean existByEmail(String email) {
+    public boolean existsByEmail(String email) {
         return iusuarioRepository.existsByEmail(email);
     }
     
