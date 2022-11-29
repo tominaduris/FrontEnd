b4076a4db56734571af930e7aa83aa309b7b7b05
tominaduris <tominaduris@hotmail.com>
2022-11-29 10:32:55 -0300

    agregado de preauthorize

--- naduristomas/BackEnd/src/main/java/com/portfolio/nt/Controller/PersonaController.java
+++ naduristomas/BackEnd/src/main/java/com/portfolio/nt/Controller/PersonaController.java
@@ -4,6 +4,7 @@ import com.portfolio.nt.Interface.IPersonaService;
 import com.portfolio.nt.entity.Persona;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,17 +20,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class PersonaController {
     @Autowired IPersonaService ipersonaService;
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping("/personas/crear")
     public String createPersona(@RequestBody Persona persona){
         ipersonaService.savePersona(persona);
         return "La persona fue creada correctamente";
     }
     
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/personas/borrar/{id}")
     public String deletePersona(@PathVariable Long id){
         ipersonaService.deletePersona(id);
@@ -37,6 +40,7 @@ public class PersonaController {
     }
     
     // URL:PUERTO/personas/editar/4/nombre & apellido & img
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/personas/editar/{id}")
     public Persona editPersona(@PathVariable Long id,
                                @RequestParam("nombre") String nuevoNombre,
