986453dcfce18c6458d64e8f68fce5fc5c8a5ea3
tominaduris <tominaduris@hotmail.com>
2022-11-29 13:23:13 -0300

    inicio de sesion en el front end

--- naduristomas/FrontEnd/src/app/app.module.ts
+++ naduristomas/FrontEnd/src/app/app.module.ts
@@ -17,6 +17,8 @@ import { FooterComponent } from './components/footer/footer.component';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { FormsModule } from '@angular/forms';
+import { interceptorProvider } from './service/interceptor-service';
 
 @NgModule({
   declarations: [
@@ -35,12 +37,15 @@ import { LoginComponent } from './components/login/login.component';
     LoginComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     NgCircleProgressModule.forRoot({}),
     HttpClientModule
   ],
-  providers: [],
+  providers: [
+    interceptorProvider
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
