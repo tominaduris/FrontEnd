6ca2824a099e05181fcf69f14a70d15c7e8cfcde
tominaduris <tominaduris@hotmail.com>
2022-11-30 19:18:58 -0300

    componente experiencia en el backend

---
+++ naduristomas/BackEnd/src/main/java/com/portfolio/nt/repository/RExperiencia.java
@@ -0,0 +1,13 @@
+
+package com.portfolio.nt.repository;
+
+import com.portfolio.nt.entity.Experiencia;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RExperiencia extends JpaRepository<Experiencia, Integer>{
+    public Optional<Experiencia> findByNombreE(String nombreE);
+    public boolean existsByNombreE(String nombreE);
+}
