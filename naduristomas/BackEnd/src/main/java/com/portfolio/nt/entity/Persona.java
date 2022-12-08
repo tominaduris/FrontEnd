70f638d5dea32dcd504eae657e5341acb1d270de
tominaduris <tominaduris@hotmail.com>
2022-12-08 17:47:14 -0300

    persona modificada en el backend

--- naduristomas/BackEnd/src/main/java/com/portfolio/nt/entity/Persona.java
+++ naduristomas/BackEnd/src/main/java/com/portfolio/nt/entity/Persona.java
@@ -10,12 +10,11 @@ import lombok.Getter;
 import lombok.Setter;
 
 
-@Getter @Setter
 @Entity 
 public class Persona {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private int id;
     
     @NotNull
     @Size(min = 1, max = 50, message = "no cumple con la longitud")
@@ -25,16 +24,63 @@ public class Persona {
     @Size(min = 1, max = 50, message = "no cumple con la longitud")
     private String apellido;
     
-    @Size(min = 1, max = 50, message = "no cumple con la longitud")
+    @NotNull
+    private String descripcion;
+    
+    
     private String img;
 
     public Persona() {
     }
 
-    public Persona(String nombre, String apellido, String img) {
+    public Persona(String nombre, String apellido, String descripcion, String img) {
         this.nombre = nombre;
         this.apellido = apellido;
+        this.descripcion = descripcion;
         this.img = img;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+    
+    
+    
 }
 
