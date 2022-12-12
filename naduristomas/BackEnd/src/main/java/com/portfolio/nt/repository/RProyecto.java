15ae05bb218ef07ffa7eb740490894b283bdcc09
tominaduris <tominaduris@hotmail.com>
2022-12-12 19:05:20 -0300

    proyectos
    creacion del elemento proyecto en el back end

---
+++ naduristomas/BackEnd/src/main/java/com/portfolio/nt/repository/RProyecto.java
@@ -0,0 +1,12 @@
+package com.portfolio.nt.repository;
+
+import com.portfolio.nt.entity.Proyecto;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RProyecto extends JpaRepository<Proyecto, Integer>{
+    public Optional<Proyecto> findByNombreP(String nombreP);
+    public boolean existsByNombreP(String nombreP);
+}
